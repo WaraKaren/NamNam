5045ea8aa0693e215f5c36d824fc1149097ef4c7
mariacortesm <mximena07@gmail.com>
2023-09-26 12:44:16 -0500

    Creación del módulo utils para las funciones de firebase

--- src/Components/register.js
+++ src/Components/register.js
@@ -3,6 +3,7 @@ import { createUserWithEmail } from '../firebaseconfig';
 const register = (navigateTo) => {
   const section = document.createElement('section');
   const logoRegister = document.createElement('img');
+  logoRegister.className = 'logoClass';
   logoRegister.src = 'imagenes/logo.png';
   section.textContent = 'Crea tu cuenta';
   const form = document.createElement('form');
@@ -37,14 +38,47 @@ const register = (navigateTo) => {
     const email = e.target.emailReg.value;
     const password = e.target.passwordReg.value;
     console.log(email, password); // nos muestra el email y clave digitado
-    createUserWithEmail(email, password)
-      .then((userCredential) => {
-        // console.log(userCredential);
-        navigateTo('/welcome');
-      })
-      .catch((error) => {
-        // console.log(error);
-      });
+    createUserWithEmail(email, password, navigateTo);
+    // .then((userCredential) => {
+    //   // console.log(userCredential);
+    //   navigateTo('/welcome');
+    // })
+    // .catch((error) => {
+    //   const errorMessage = error.message;
+
+    //   // nos muestra el error que tiene el usuario en la consola
+    //   switch (errorMessage) {
+    //     case 'Firebase: Error (auth/email-already-in-use).':
+    //       alert('Este correo ya está en uso');
+    //       break;
+    //     case 'Firebase: Error (auth/passwords-not-match).':
+    //       alert('The passwords don\'t match');
+    //       break;
+    //     case 'Firebase: Password should be at least 6 characters (auth/weak-password).':
+    //       alert('La contraseña debe contener mínimo 6 caracteres');
+    //       break;
+    //     case 'Firebase: Error (auth/invalid-email).':
+    //       alert('Correo inválido');
+    //       break;
+    //     case 'Firebase: Error (auth/user-not-found).':
+    //       alert('User not found');
+    //       break;
+    //     case 'Firebase: error (auth/wrong-password).':
+    //       alert('Wrong password');
+    //       break;
+    //     case 'Firebase: Error (auth/too-many-requests).':
+    //       alert('Too many requests. Try again later');
+    //       break;
+    //     case 'Firebase: Error (auth/cancelled-popup-request)':
+    //       alert('The request was canceld');
+    //       break;
+    //     // case 'Firebase: Error (auth/popup-blocked).':
+    //     //   alert('the popup was blocked');
+    //     //   break;
+    //     default:
+    //       console.log('default');
+    //   }
+    // });
     // const mensaje = 'firebase ';
     // switch (mensaje) {
     //   case 'Firebase: Error (auth/email-already-in-use).':
