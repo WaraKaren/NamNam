be3e4e49e53b83d59044f9c2efa5669c98bafd72
WaraKaren <wkpacajesm@gmail.com>
2023-09-26 18:41:31 -0400

    cambios en welcome y css welcome

--- src/Components/welcome.js
+++ src/Components/welcome.js
@@ -1,17 +1,29 @@
 const welcome = (navigateTo) => {
   const section = document.createElement('section');
+  section.className = 'containerWelcome';
+  const divOne = document.createElement('div');
+  divOne.className = 'divOneWelcome';
   const logo = document.createElement('img');
+  logo.className = 'logoWelcome';
   logo.src = 'imagenes/logo.png';
-  section.textContent = '¡Usuaria registrada exitosamente!';
-  const titleWelcome = document.createElement('h1');
+  const mensajeWelcome = document.createElement('h1');
+  mensajeWelcome.className = 'mensajeWelcomw';
+  mensajeWelcome.textContent = '¡Usuaria registrada exitosamente!';
+  const titleWelcome = document.createElement('h2');
   titleWelcome.textContent = 'Bienvenida a Ñam Ñam';
-  titleWelcome.className = 'titleWelcomeClass';
+  titleWelcome.className = 'titleWelcome';
+  const divTwo = document.createElement('div');
+  divTwo.className = 'divTwoWelcome';
   const buttonHome = document.createElement('button');
+  buttonHome.className = 'buttonHomeWelcome';
   buttonHome.textContent = 'Siguiente';
 
   buttonHome.addEventListener('click', () => navigateTo('/'));
 
-  section.append(logo, titleWelcome, buttonHome);
+  section.append(divOne, divTwo);
+  divOne.append(logo, mensajeWelcome, titleWelcome);
+  divTwo.append(buttonHome);
+
   return section;
 };
 export default welcome;
