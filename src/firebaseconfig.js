9288075a851641a92e05c6932a0d21aa625351ee
alexaolalde <alejandra.olalde1817@gmail.com>
2023-09-28 15:57:22 -0600

    Se crea la funcionalidad de Login y se liga con Firebase, ademas se hace el CSS de Home y Login

--- src/firebaseconfig.js
+++ src/firebaseconfig.js
@@ -3,7 +3,6 @@
 import { initializeApp } from 'firebase/app';
 import {
   getAuth,
-  // signInWithEmailAndPassword,
   // onAuthStateChanged,
 } from 'firebase/auth';
 // TODO: Replace the following with your app's Firebase project configuration
@@ -19,17 +18,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
-
-// signInWithEmailAndPassword(auth, email, password)
-//   .then((userCredential) => {
-//     // Signed in
-//     const user = userCredential.user;
-//     // ...
-//   })
-//   .catch((error) => {
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//   });
 
 // onAuthStateChanged(auth, (user) => {
 //   if (user) {
